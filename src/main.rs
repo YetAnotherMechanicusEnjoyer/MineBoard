2a8e1d3ead9450689e40217b0cc50ba2d320f29a
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-11-25 00:58:33 +0100

    [UPDATE] main.rs
    Add "/ws/logs" route

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
 use std::sync::Mutex;
 
+use actix::Actor;
 use actix_files::Files;
 use actix_web::{App, HttpServer, web};
 
+use crate::broadcaster::Broadcaster;
+
+mod broadcaster;
+mod messages;
 mod server;
+mod websocket;
 
 const FRONT_BUILD_DIR: &str = "./front/dist";
 
 #[derive(Debug)]
 pub struct AppState {
     pub server_pid: Mutex<Option<u32>>,
+    pub broadcaster: actix::Addr<Broadcaster>,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let broadcaster_addr = Broadcaster::new().start();
+
     let app_state = web::Data::new(AppState {
         server_pid: Mutex::new(None),
+        broadcaster: broadcaster_addr,
     });
 
     println!("Ready !");
@@ -28,6 +38,7 @@ async fn main() -> std::io::Result<()> {
                     .route("/start", web::post().to(server::start_server))
                     .route("/stop", web::post().to(server::stop_server)),
             )
+            .service(web::scope("/ws").route("/logs", web::get().to(websocket::ws_route)))
             .service(Files::new("/", FRONT_BUILD_DIR).index_file("index.html"))
     })
     .bind(("0.0.0.0", 8080))?
